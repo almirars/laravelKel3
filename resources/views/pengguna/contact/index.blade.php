135ff3a47f42da6b7d25f0d5e7d5c5024b08dd7c
Syahrul Riza <syahrulandisan@gmail.com>
2022-09-07 14:47:11 +0700

    first commit

---
+++ resources/views/pengguna/contact/index.blade.php
@@ -0,0 +1,48 @@
+@extends('pengguna.layout.main')
+@section('konten')
+    <div id="plant" class="contact_us layout_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 ">
+                    <div class="titlepage">
+                        <h2 style="text-align: center; margin-top: 45px;">Contact <strong
+                                style="color: #111315;">Us</strong></h2>
+                        <span style="text-align: center;">available, but the majority have suffered alteration in some form,
+                            by injected randomised words which don't look even slightly believable</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contact_us_2 layout_padding paddind_bottom_0">
+        <div class="container">
+            <div class="row pb-5">
+                <div class="col-md-6">
+                    <div class="soc_text">soC</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="email_btn">
+                        <form action="/action_page.php">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Name" name="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Email" name="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Phone" name="Phone">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Massage" name="text3">
+                            </div>
+                            <div class="submit_btn">
+                                <button type="submit" class="btn btn-primary" style="background: #081b30;">Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
