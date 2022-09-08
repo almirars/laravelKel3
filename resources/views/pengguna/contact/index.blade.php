1b9ce50d15f614b552d4933e04f163f01f24ad85
almirars <almirasabitaaa@gmail.com>
2022-09-08 09:11:12 +0700

    pengaduan update

--- resources/views/pengguna/contact/index.blade.php
+++ resources/views/pengguna/contact/index.blade.php
@@ -5,9 +5,9 @@
             <div class="row">
                 <div class="col-md-12 ">
                     <div class="titlepage">
-                        <h2 style="text-align: center; margin-top: 145px;">Contact <strong
-                                style="color: #111315;">Us</strong></h2>
-                        <span style="text-align: center;">Gunung Lawu</span>
+                        <h2 style="text-align: center; margin-top: 145px;">Form Aduan <strong
+                                style="color: #111315;">Masyarakat</strong></h2>
+                        <span style="text-align: center;">Identitas pelapor dijamin kerahasiaannya</span>
                     </div>
                 </div>
             </div>
@@ -16,11 +16,8 @@
 
     <div class="contact_us_2 layout_padding paddind_bottom_0">
         <div class="container">
-            <div class="row pb-5">
-                <div class="col-md-6">
-                    <div class="soc_text">soC</div>
-                </div>
-                <div class="col-md-6">
+            <div class="row pb-5 justify-content-center">
+                <div class="col-md-9">
                     <div class="email_btn">
                         <form action="/action_page.php">
                             <div class="form-group">
@@ -33,10 +30,12 @@
                                 <input type="text" class="form-control form-control-sm" placeholder="Phone" name="Phone">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-sm" placeholder="Massage" name="text3">
+                                <textarea type="text" class="form-control form-control-sm" placeholder="Massage"
+                                    name="text3"></textarea>
                             </div>
                             <div class="submit_btn">
-                                <button type="submit" class="btn btn-primary" style="background: #081b30;">Send</button>
+                                <button type="submit" class="btn btn-primary"
+                                    style="background: #081b30; color: #fff; padding: 11px;">Send</button>
                             </div>
                         </form>
                     </div>
